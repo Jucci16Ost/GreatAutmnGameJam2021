57c9b19dc89e3778332717525cca08752c7b3f9a
John Ucci <jucci@ostusa.com>
2021-09-19 14:21:43 -0400

    Fixed overlay bug.

--- Great Autumn Game Jam 2021/Assets/Scripts/GameOverController.cs
+++ Great Autumn Game Jam 2021/Assets/Scripts/GameOverController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Constants;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +10,8 @@ namespace Assets.Scripts
     {
         public void Show() {
             gameObject.SetActive(true);
+            var inGameView = GameObject.Find(UiConstants.InGameViewName);
+            inGameView?.SetActive(false);
         }
 
         public void TryAgainButton() {
