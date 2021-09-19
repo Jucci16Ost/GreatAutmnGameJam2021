f28b7d8a35d4463b59b681b94d7519f841e2b828
John Ucci <jucci@ostusa.com>
2021-09-19 11:21:54 -0400

    Created creature spawning system.

---
+++ Great Autumn Game Jam 2021/Assets/Scripts/Constants/SceneConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Constants
+{
+    public static class SceneConstants
+    {
+        /// <summary>
+        /// Name of Maze Easy one Scene
+        /// </summary>
+        public const string MazeEasy1 = "Maze_Easy_1";
+
+        /// <summary>
+        /// Name of Maze Easy two Scene
+        /// </summary>
+        public const string MazeEasy2 = "Maze_Easy_2";
+
+        /// <summary>
+        /// Name of Maze Easy three Scene
+        /// </summary>
+        public const string MazeEasy3 = "Maze_Easy_3";
+    }
+}
